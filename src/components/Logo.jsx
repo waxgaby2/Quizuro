8d80b814087740fd2ac8d56279cbc76bc7ccb247
waxgaby2 <abolajitimothy414@gmail.com>
2026-04-14 20:32:52 +0100

    Updated Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 //Logo
 export function Logo(){
-    return (<div className="relative w-[100%] flex justify-center items-center text-white m-2 p-2">
+    return (<div className="relative flex justify-center items-center text-white m-2 p-2">
     <h1 className="text-white font-bold! tracking-normal!">Quizuro
         <span className="text-amber-400">?</span>
     </h1>
-    <p className="absolute top-[51px] sm:left-[48.7%] max-sm:left-[46.8%] text-[8px]">Quizzes by WaxGaby</p>
+    <p className="absolute top-[51px] left-[80px] text-[8px]">Quizzes by WaxGaby</p>
     </div>)
 }
