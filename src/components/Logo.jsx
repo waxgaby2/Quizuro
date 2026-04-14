3bd546467205675de8173e37ec1690a071635909
waxgaby2 <abolajitimothy414@gmail.com>
2026-04-14 19:53:29 +0100

    update favicon and logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
 //Logo
 export function Logo(){
     return (<div className="relative w-[100%] flex justify-center items-center text-white m-2 p-2">
-    <h1 className="text-white font-bold! tracking-normal!">Quizuro</h1>
-    <p className="absolute top-[51px] sm:left-[49.2%] max-sm:left-[48%] text-[8px]">Quizzes by WaxGaby</p>
+    <h1 className="text-white font-bold! tracking-normal!">Quizuro
+        <span className="text-amber-400">?</span>
+    </h1>
+    <p className="absolute top-[51px] sm:left-[48.7%] max-sm:left-[46.8%] text-[8px]">Quizzes by WaxGaby</p>
     </div>)
 }
